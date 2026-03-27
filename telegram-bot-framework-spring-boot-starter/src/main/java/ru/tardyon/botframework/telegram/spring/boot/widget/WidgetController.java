897ad0b320a633cfd312bf10bdbed88e4cf42b23
sergej <tardyon@yandex.ru>
2026-03-27 08:17:46 -0700

    Register screen and widget controllers as Spring components

--- telegram-bot-framework-spring-boot-starter/src/main/java/ru/tardyon/botframework/telegram/spring/boot/widget/WidgetController.java
+++ telegram-bot-framework-spring-boot-starter/src/main/java/ru/tardyon/botframework/telegram/spring/boot/widget/WidgetController.java
@@ -5,9 +5,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Component
 public @interface WidgetController {
+
+    @AliasFor(annotation = Component.class)
+    String value() default "";
 }
